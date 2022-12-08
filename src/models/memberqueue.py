3a3327776f0d3703a90fedc1825c6e2331c95261
Andrew Hong <andrewhong@myyahoo.com>
2022-12-08 11:12:41 -0500

    Add code & expiration to constructor

--- src/models/memberqueue.py
+++ src/models/memberqueue.py
@@ -1,4 +1,9 @@
+from config import EMAIL_CODE_EXPIRE
+
+
 class MemberQueue:
-    def __init__(self, email: str, netid: str) -> None:
+    def __init__(self, email: str, netid: str, code: str) -> None:
         self.email = email
         self.netid = netid
+        self.code = code
+        self.expire = EMAIL_CODE_EXPIRE
