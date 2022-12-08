27ee5e68ade0f1d6e296fd97f1cdd46450916e85
Andrew Hong <andrewhong@myyahoo.com>
2022-12-08 03:41:33 -0500

    Remove sensitive information (Password and token has been reset)

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
-TOKEN: str = "MTA1MDIxOTIwNTEzMzM0NDkyMg.G3sCjA.CVQuLFTZ4uiJgWM5U3PGxDe-ZHpD0mFAzyiLtA"
-BOT_EMAIL: str = "rutgersverifybot@outlook.com"
-BOT_EMAIL_PWD: str = "lmao1234*"
+TOKEN: str = ""
+BOT_EMAIL: str = ""
+BOT_EMAIL_PWD: str = ""
 
 EMAIL_CODE_LENGTH: int = 10
 EMAIL_CODE_EXPIRE: int = 60
