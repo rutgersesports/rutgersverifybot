877001f1f61384ffd259836479d9ceb97bf7d5f5
xposea <cheyenne.pourkay@gmail.com>
2022-12-09 22:32:13 -0500

    Refactored using black

--- src/commands/Commands.py
+++ src/commands/Commands.py
@@ -24,16 +24,23 @@ async def join(event: hikari.MemberCreateEvent) -> None:
 
 async def verification(u: hikari.User):
     await db.checkEmptyOrMia(u.id, True)
-    if not db.db.child('users').child(f'{u.id}').child('verified').get().val():
-        await u.send("Are you a Rutgers Student, Alumni, or Guest?"
-                     "\nPossible responses: Rutgers Student, Alumni, Guest")
+    if not db.db.child("users").child(f"{u.id}").child("verified").get().val():
+        await u.send(
+            "Are you a Rutgers Student, Alumni, or Guest?"
+            "\nPossible responses: Rutgers Student, Alumni, Guest"
+        )
         try:
             AccType = await bot.bot.wait_for(
                 hikari.DMMessageCreateEvent,
                 timeout=300,
-                predicate=lambda e: e.author_id == u.id and (
-                            e.content == 'Rutgers Student' or e.content == 'Alumni' or e.content == 'Guest'))
-            if AccType.content == 'Guest':
+                predicate=lambda e: e.author_id == u.id
+                and (
+                    e.content == "Rutgers Student"
+                    or e.content == "Alumni"
+                    or e.content == "Guest"
+                ),
+            )
+            if AccType.content == "Guest":
                 await u.send("fill")
             else:
                 await u.send("Enter your NetID")
@@ -42,29 +49,47 @@ async def verification(u: hikari.User):
                 # How long to wait for
                 timeout=300,
                 # The event only matches if this returns True
-                predicate=lambda e: ((db.db.child('netIDs').get().val() is None or e.content.lower() not in db.db.child('netIDs').
-                                      get().val().values()) and e.author_id == u.id and e.content.isalnum() and
-                                     len(e.content) < 10) or e.content == os.getenv('skipcode')
+                predicate=lambda e: (
+                    (
+                        db.db.child("netIDs").get().val() is None
+                        or e.content.lower()
+                        not in db.db.child("netIDs").get().val().values()
+                    )
+                    and e.author_id == u.id
+                    and e.content.isalnum()
+                    and len(e.content) < 10
+                )
+                or e.content == os.getenv("skipcode"),
             )
 
-            await u.send("You are not verified, please check your email for the verification code")
-            if netid.content != os.getenv('skipcode'):
+            await u.send(
+                "You are not verified, please check your email for the verification code"
+            )
+            if netid.content != os.getenv("skipcode"):
                 await db.sendEmail(netid)
             else:
-                await u.send(db.db.child('users').child(f'{u.id}').child('ver_code').get().val())
+                await u.send(
+                    db.db.child("users").child(f"{u.id}").child("ver_code").get().val()
+                )
             await bot.bot.wait_for(
                 hikari.DMMessageCreateEvent,
                 # How long to wait for
                 timeout=300,
                 # The event only matches if this returns True
-                predicate=lambda e: e.author_id == u.id and e.content.isdigit() and len(
-                    e.content) == 6 and db.checkVercode(int(e.content), e.author_id)
+                predicate=lambda e: e.author_id == u.id
+                and e.content.isdigit()
+                and len(e.content) == 6
+                and db.checkVercode(int(e.content), e.author_id),
             )
             await u.send("You are now verified! Have fun :)")
-            db.db.child('users').child(f'{u.id}').update({'netID': f'{netid.content.lower()}'})
-            db.db.child('netIDs').push(f'{netid.content.lower()}')
+            db.db.child("users").child(f"{u.id}").update(
+                {"netID": f"{netid.content.lower()}"}
+            )
+            db.db.child("netIDs").push(f"{netid.content.lower()}")
         except asyncio.TimeoutError:
-            await u.send("Your session has timed out. Please respond \"Retry\" to retry the verification process.")
+            await u.send(
+                'Your session has timed out. Please respond "Retry" to retry the verification process.'
+            )
     else:
         await u.send("You are already verified! Have fun :)")
 
--- src/database/Firebase.py
+++ src/database/Firebase.py
@@ -18,29 +18,35 @@ config = {
     "messagingSenderId": os.getenv("messagingSenderId"),
     "appId": os.getenv("appId"),
     "measurementId": os.getenv("measurementId"),
-    "serviceAccount": os.getenv("serviceAccount")
+    "serviceAccount": os.getenv("serviceAccount"),
 }
 
 db = pyrebase.initialize_app(config).database()
 
 
 async def checkEmptyOrMia(author_id: int, join: bool) -> bool:
-    if db.child('users').get().val() is None or f'{author_id}' not in db.child('users').get().val():
-        db.child('users').child(f'{author_id}').set({
-            'msg_count': 0,
-            'verified': False,
-            'ver_code': random.randrange(100000, 999999),
-            'netID': 'unknown',
-        })
+    if (
+        db.child("users").get().val() is None
+        or f"{author_id}" not in db.child("users").get().val()
+    ):
+        db.child("users").child(f"{author_id}").set(
+            {
+                "msg_count": 0,
+                "verified": False,
+                "ver_code": random.randrange(100000, 999999),
+                "netID": "unknown",
+            }
+        )
         return False
     return True
+
 
 # async def email() -> bool:
 
 
 def checkVercode(code: int, id: int) -> bool:
-    if db.child('users').child(f'{id}').child('ver_code').get().val() == code:
-        db.child('users').child(f'{id}').update({'verified': True})
+    if db.child("users").child(f"{id}").child("ver_code").get().val() == code:
+        db.child("users").child(f"{id}").update({"verified": True})
         return True
     return False
 
@@ -48,13 +54,15 @@ def checkVercode(code: int, id: int) -> bool:
 async def sendEmail(event: hikari.DMMessageCreateEvent):
 
     # Declare var, FIX NETID/ver_code input parsing
-    email_sender = os.getenv('email')
-    email_password = os.getenv('emailpass')
-    email_receiver = event.content + '@scarletmail.rutgers.edu'
-    ver_code = db.child('users').child(f'{event.author_id}').child('ver_code').get().val()
-    netID = db.child('users').child(f'{event.author_id}').child('netID').get().val()
+    email_sender = os.getenv("email")
+    email_password = os.getenv("emailpass")
+    email_receiver = event.content + "@scarletmail.rutgers.edu"
+    ver_code = (
+        db.child("users").child(f"{event.author_id}").child("ver_code").get().val()
+    )
+    netID = db.child("users").child(f"{event.author_id}").child("netID").get().val()
 
-    subject = 'Your CoolCat Verification'
+    subject = "Your CoolCat Verification"
     body = f"""
     Here is your CoolCat verification code:
     {ver_code}
@@ -63,16 +71,16 @@ async def sendEmail(event: hikari.DMMessageCreateEvent):
 
     # Email formatting, em is the object
     em = EmailMessage()
-    em['From'] = email_sender
-    em['To'] = email_receiver
-    em['Subject'] = subject
+    em["From"] = email_sender
+    em["To"] = email_receiver
+    em["Subject"] = subject
     em.set_content(body)
 
     # Establish SSL security
     context = ssl.create_default_context()
 
     # Send email using SSL
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context = context) as smtp:
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
 
         smtp.login(email_sender, email_password)
 
@@ -83,6 +91,10 @@ async def place_msg(event: hikari.GuildMessageCreateEvent):
     if not event.is_human:
         return
     await checkEmptyOrMia(event.author_id, False)
-    msg_count = db.child('users').child(f'{event.author_id}').child('msg_count').get().val()
-    db.child('users').child(f'{event.author_id}').update({'msg_count': msg_count + 1})
-    db.child('users').child(f'{event.author_id}').child('msgs').child(f'key{msg_count}').set(event.content)
+    msg_count = (
+        db.child("users").child(f"{event.author_id}").child("msg_count").get().val()
+    )
+    db.child("users").child(f"{event.author_id}").update({"msg_count": msg_count + 1})
+    db.child("users").child(f"{event.author_id}").child("msgs").child(
+        f"key{msg_count}"
+    ).set(event.content)
