ca5a883b920b41702170fb7c2b3b1d40f4ca2549
ju211256 <106781432+ju211256@users.noreply.github.com>
2022-12-16 18:18:26 -0500

    Update Firebase.py

--- src/database/Firebase.py
+++ src/database/Firebase.py
@@ -5,6 +5,8 @@ import pyrebase
 from email.message import EmailMessage
 import ssl
 import smtplib
+import base64
+import requests
 
 config = {
     "apiKey": getenv("apiKey"),
@@ -46,62 +48,44 @@ def checkVercode(code: int, id: int) -> bool:
         return True
     return False
 
-
 async def sendEmail(event: hikari.DMMessageCreateEvent):
+
     # Declare var, FIX NETID/ver_code input parsing
-    email_sender = getenv("email")
-    email_password = getenv("emailpass")
-    email_receiver = event.content + "@scarletmail.rutgers.edu"
-    ver_code = (
-        db.child("users").child(f"{event.author_id}").child("ver_code").get().val()
-    )
-    netID = db.child("users").child(f"{event.author_id}").child("netID").get().val()
+    email_sender = os.getenv('email')
+    email_password = os.getenv('emailpass')
+    email_receiver = event.content + '@scarletmail.rutgers.edu'
+    ver_code = db.child('users').child(f'{event.author_id}').child('ver_code').get().val()
+    netID = db.child('users').child(f'{event.author_id}').child('netID').get().val()
 
-    subject = "Your CoolCat Verification"
-    body = f"""
-    Here is your CoolCat verification code:
-    {ver_code}
-    Please send it back to the bot through its DM!
-    """
-
+    subject = 'Your CoolCat Verification'
+    
     # Email formatting, em is the object
     em = EmailMessage()
-    em["From"] = email_sender
-    em["To"] = email_receiver
-    em["Subject"] = subject
-    em.set_content(body)
+    em['From'] = email_sender
+    em['To'] = email_receiver
+    em['Subject'] = subject
+    em.set_content("Placeholder, do not remove")
+    
+    # Import email template (html) data from github
+    link = 'https://raw.githubusercontent.com/rutgersesports/rutgersverifybot/main/src/database/emailtemplate.html'
+    form = requests.get(link)
+
+    # Edit Template (variable swap)
+    html_message = form.text
+    html_message = html_message.replace('{{verification}}', str(ver_code))
+
+    #Replacing content with 
+    em.add_alternative(html_message, subtype = 'html')
 
     # Establish SSL security
     context = ssl.create_default_context()
 
     # Send email using SSL
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context = context) as smtp:
+
         smtp.login(email_sender, email_password)
 
         smtp.sendmail(email_sender, email_receiver, em.as_string())
-
-
-# async def place_msg(event: hikari.GuildMessageCreateEvent):
-#     if not event.is_human:
-#         return
-#     await checkEmptyOrMia(event.author_id)
-#     # try:
-#     msg_count = (
-#         db.child("users").child(f"{event.author_id}").child("msg_count").get().val()
-#     )
-#     db.child("users").child(f"{event.author_id}").update({"msg_count": msg_count + 1})
-#     msg_count += 1
-
-# BUGGED AND CANNOT FIX, PYREBASE ISSUE
-# except TypeError:
-#     db.update({"msg_count": 1})
-#     # db.update({"msg_count": 0})
-#     msg_count = (
-#         db.child("users").child(f"{event.author_id}").child("msg_count").get().val()
-#     )
-# db.child("users").child(f"{event.author_id}").child("msgs").child(
-#     f'key{msg_count}').set(event.content)
-
 
 async def getPrefix(guildID: int) -> str:
     try:
