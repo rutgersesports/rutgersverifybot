c1bb4439f96384aa72581243cc99cef9e8fabc76
xposea <cheyenne.pourkay@gmail.com>
2022-12-17 01:14:23 -0500

    Adding email css/html + updates to database

--- src/database/firebase.py
+++ src/database/firebase.py
@@ -7,6 +7,8 @@ from email.message import EmailMessage
 import ssl
 import smtplib
 
+import requests
+
 config = {
     "apiKey": getenv("apiKey"),
     "authDomain": getenv("authDomain"),
@@ -48,34 +50,41 @@ def check_vercode(code: int, user_id: int) -> bool:
     return False
 
 
-async def send_email(netid: str, author_id: int):
+async def send_email(netid: str, author_id: int) -> None:
+
     # Declare var, FIX NETID/ver_code input parsing
     email_sender = getenv("email")
     email_password = getenv("emailpass")
-    email_receiver = f"{netid}@scarletmail.rutgers.edu"
+    email_receiver = netid + "@scarletmail.rutgers.edu"
     ver_code = random.randrange(100000, 999999)
     db.child("users").child(f"{author_id}").child("ver_code").set(ver_code)
-    # netID = db.child("users").child(f"{event.author_id}").child("netID").get().val()
 
     subject = "Your CoolCat Verification"
-    body = f"""
-    Here is your CoolCat verification code:
-    {ver_code}
-    Please send it back to the bot through its DM!
-    """
 
     # Email formatting, em is the object
     em = EmailMessage()
     em["From"] = email_sender
     em["To"] = email_receiver
     em["Subject"] = subject
-    em.set_content(body)
+    em.set_content("Placeholder, do not remove")
+
+    # Import email template (html) data from GitHub
+    link = "https://raw.githubusercontent.com/rutgersesports/rutgersverifybot/main/src/database/emailtemplate.html"
+    form = requests.get(link)
+
+    # Edit Template (variable swap)
+    html_message = form.text
+    html_message = html_message.replace("{{verification}}", str(ver_code))
+
+    # Replacing content with
+    em.add_alternative(html_message, subtype="html")
 
     # Establish SSL security
     context = ssl.create_default_context()
 
     # Send email using SSL
     with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
+
         smtp.login(email_sender, email_password)
 
         smtp.sendmail(email_sender, email_receiver, em.as_string())
